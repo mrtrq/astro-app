991e416ebb37b719993e0ec1c083b2b99e860dfb
mrtrq <tarreq.maulana@gmail.com>
2024-07-23 12:07:49 +0700

    add: greeting banner

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://master--elaborate-daffodil-7aaf14.netlify.app/"
+  site: "https://master--elaborate-daffodil-7aaf14.netlify.app/",
+  integrations: [preact()]
 });
